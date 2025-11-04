37295dcb40731586ebce2ecf5c8b7409e469eabb
SAIBABU2997 <saibabusadanala78@gmail.com>
2025-11-04 12:27:12 +0530

    Remove .env file & add to gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect("mongodb://localhost:27017/mean-notes")
-.then(() => console.log("✅ MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("✅ MongoDB Atlas Connected"))
 .catch(err => console.log("❌ DB Error:", err));
 
 // Routes
@@ -18,5 +19,5 @@ const notesRoute = require("./routes/noteRoutes");
 app.use("/api/notes", notesRoute);
 
 // Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
